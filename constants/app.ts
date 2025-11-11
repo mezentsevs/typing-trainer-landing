ae9e92644abd54dd8a81142bfcffbb5ab8d51d4b
mezentsevs <mezentsevs@rambler.ru>
2025-11-11 19:35:53 +0400

    feat: add app constants

---
+++ constants/app.ts
@@ -0,0 +1,3 @@
+export const APP_NAME: string = 'Typing Trainer';
+export const APP_SLOGAN: string =
+    'Master the art of blind typing: code like a hacker, write like a novelist, report like a journalist.';
