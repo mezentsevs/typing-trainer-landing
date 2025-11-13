b3ad1cca02e104e523aa62ae3aee0bf34221cf18
mezentsevs <mezentsevs@rambler.ru>
2025-11-13 21:34:50 +0400

    refactor: update PrimaryButton component

--- components/uikit/PrimaryButton.vue
+++ components/uikit/PrimaryButton.vue
@@ -1,15 +1,7 @@
 <template>
-    <a
-        :href
-        class="inline-flex items-center justify-center bg-gradient-to-r from-blue-600 to-purple-600 text-white font-medium rounded-lg transition-all duration-200 hover:shadow-lg hover:from-blue-700 hover:to-purple-700 active:shadow-2xl active:from-blue-800 active:to-purple-800 active:shadow-blue-500/50">
+    <BaseButton
+        v-bind="$attrs"
+        class="bg-gradient-to-r from-blue-600 to-purple-600 hover:from-blue-700 hover:to-purple-700 active:from-blue-800 active:to-purple-800 active:shadow-blue-500/50">
         <slot />
-    </a>
+    </BaseButton>
 </template>
-
-<script setup lang="ts">
-interface PrimaryButtonProps {
-    href: string;
-}
-
-defineProps<PrimaryButtonProps>();
-</script>
