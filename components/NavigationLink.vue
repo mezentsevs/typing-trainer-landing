9f9d36fa57df5017832bbe5d2b36b613c1fbc67d
mezentsevs <mezentsevs@rambler.ru>
2025-11-13 12:22:41 +0400

    feat: add NavigationLink component

---
+++ components/NavigationLink.vue
@@ -0,0 +1,15 @@
+<template>
+    <a
+        :href="href"
+        class="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-cyan-400 transition-colors duration-200 font-medium">
+        <slot />
+    </a>
+</template>
+
+<script setup lang="ts">
+interface NavigationLinkProps {
+    href: string;
+}
+
+defineProps<NavigationLinkProps>();
+</script>
