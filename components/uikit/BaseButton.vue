5ddf58941ee8f3eaeaccb8699b7af7783b42917c
mezentsevs <mezentsevs@rambler.ru>
2025-11-13 21:33:55 +0400

    feat: add BaseButton component

---
+++ components/uikit/BaseButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <a
+        v-bind="$attrs"
+        class="inline-flex items-center justify-center text-white font-medium rounded-lg transition-all duration-200 hover:shadow-lg active:shadow-2xl">
+        <slot />
+    </a>
+</template>
