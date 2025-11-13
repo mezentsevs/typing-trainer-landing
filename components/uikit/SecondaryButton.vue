f714b14d1ec2981bb0193ebb4c60aecfcfde28ce
mezentsevs <mezentsevs@rambler.ru>
2025-11-13 21:37:26 +0400

    feat: add SecondaryButton and SuccessButton components

---
+++ components/uikit/SecondaryButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <BaseButton
+        v-bind="$attrs"
+        class="bg-gradient-to-r from-gray-600 to-gray-700 hover:from-gray-700 hover:to-gray-800 active:from-gray-800 active:to-gray-900 active:shadow-gray-500/50">
+        <slot />
+    </BaseButton>
+</template>
---
+++ components/uikit/SuccessButton.vue
@@ -0,0 +1,7 @@
+<template>
+    <BaseButton
+        v-bind="$attrs"
+        class="bg-gradient-to-r from-green-600 to-emerald-600 hover:from-green-700 hover:to-emerald-700 active:from-green-800 active:to-emerald-800 active:shadow-green-500/50">
+        <slot />
+    </BaseButton>
+</template>
